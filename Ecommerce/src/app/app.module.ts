b1b6f7943f243f119c58b29fb1a948ec963cd137
Gitesh Sharma <gitesh15796@gmail.com>
2022-01-25 18:30:10 +0530

    angular installed

---
+++ Ecommerce/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PublicComponent } from './public/public.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    PublicComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
