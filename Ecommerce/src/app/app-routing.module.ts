9fd5eb08f9c97b1871b30ee4963c79aa999a661f
Gitesh Sharma <gitesh15796@gmail.com>
2022-01-25 19:07:14 +0530

    public routing setup done
    (cherry picked from commit 5ae7244f7459853e312a1c2f271d381029cf0de7)

--- Ecommerce/src/app/app-routing.module.ts
+++ Ecommerce/src/app/app-routing.module.ts
@@ -1,13 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicComponent } from './public/public.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: 'home', pathMatch: 'full' },
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
-  },
-];
+  { path: '',   redirectTo: '/public', pathMatch: 'full' },
+  { path: 'public', component: PublicComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
