6052b5f95d6c485495d8689aedbec13bd2771e9b
Gitesh Sharma <gitesh15796@gmail.com>
2022-02-03 23:07:05 +0530

    public component removed and home routing done

--- Ecommerce/src/app/app-routing.module.ts
+++ Ecommerce/src/app/app-routing.module.ts
@@ -1,13 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PublicComponent } from './public/public.component';
 
 const routes: Routes = [
-  { path: '',   redirectTo: 'public', pathMatch: 'full' },
-  { path: 'public', component: PublicComponent },
+  { path: '',   redirectTo: 'home', pathMatch: 'full' },
   {
-    path: 'public',
-    loadChildren: () => import('./public/public.module').then(m => m.PublicModule)
+    path: 'home',
+    loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
   },
 ];
 
--- Ecommerce/src/app/app.module.ts
+++ Ecommerce/src/app/app.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { PublicComponent } from './public/public.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
