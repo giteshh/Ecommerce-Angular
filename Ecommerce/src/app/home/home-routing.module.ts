047fc16c0c0aa4f6cf992e457e9fa3b146e86afb
Gitesh Sharma <gitesh15796@gmail.com>
2022-02-04 19:56:11 +0530

    header component separated from home

--- Ecommerce/src/app/home/home-routing.module.ts
+++ Ecommerce/src/app/home/home-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
+import { HeaderComponent } from './pages/header/header.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent, children: [
       {path: '', redirectTo: 'home', pathMatch: 'full'},
+      { path: 'header', component: HeaderComponent },
 
     ]
   }
--- Ecommerce/src/app/home/home.module.ts
+++ Ecommerce/src/app/home/home.module.ts
@@ -1,11 +1,16 @@
+import { HomeComponent } from './home.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
+import { HeaderComponent } from './pages/header/header.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule,
     HomeRoutingModule
