57b95f0a2226ac7c9b4ad9f9951574ff237bc2c7
Gitesh Sharma <gitesh15796@gmail.com>
2022-02-04 19:45:05 +0530

    navbar added and picture added to pics folder

--- Ecommerce/src/app/app.module.ts
+++ Ecommerce/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -10,7 +9,7 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
