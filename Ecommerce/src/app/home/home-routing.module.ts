bc3edddf7c3ef295698ec72e919ddfc138a67617
Gitesh Sharma <gitesh15796@gmail.com>
2022-02-05 19:52:18 +0530

    product menu created and card added to product menu page

--- Ecommerce/src/app/home/home-routing.module.ts
+++ Ecommerce/src/app/home/home-routing.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home.component';
 import { HeaderComponent } from './pages/header/header.component';
+import {ProductMenuComponent} from "./pages/product-menu/product-menu.component";
+import {HomePageComponent} from "./pages/home-page/home-page.component";
 
 const routes: Routes = [
-  {
-    path: '',
+{
+  path: '',
     component: HomeComponent, children: [
-      {path: '', redirectTo: 'home', pathMatch: 'full'},
-      { path: 'header', component: HeaderComponent },
+  {path: '', redirectTo: 'Home', pathMatch: 'full'},
+  {path: 'Home', component: HomePageComponent},
+  {path: 'header', component: HeaderComponent},
+  {path: 'productMenu', component: ProductMenuComponent},
 
-    ]
-  }
+]
+}
 ];
 
 @NgModule({
--- Ecommerce/src/app/home/home.module.ts
+++ Ecommerce/src/app/home/home.module.ts
@@ -4,18 +4,21 @@ import { CommonModule } from '@angular/common';
 
 import { HomeRoutingModule } from './home-routing.module';
 import { HeaderComponent } from './pages/header/header.component';
-import { ProductMenuComponent } from './pages/product-menu/product-menu.component';
+import {ProductMenuComponent} from "./pages/product-menu/product-menu.component";
+import { HomePageComponent } from './pages/home-page/home-page.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     HeaderComponent,
-    ProductMenuComponent
+    ProductMenuComponent,
+    HomePageComponent
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule
+    HomeRoutingModule,
+    CommonModule
   ]
 })
 export class HomeModule { }
