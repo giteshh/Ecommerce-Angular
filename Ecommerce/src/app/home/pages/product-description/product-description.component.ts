44bb85b735cd34f509be822175a59aebd8e0b82f
Gitesh Sharma <gitesh15796@gmail.com>
2022-02-07 19:14:55 +0530

    product description modified and button set to disabled on home page

--- Ecommerce/src/app/home/pages/product-description/product-description.component.ts
+++ Ecommerce/src/app/home/pages/product-description/product-description.component.ts
@@ -7,6 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDescriptionComponent implements OnInit {
 
+  productDesc: any = [
+    {'id':1, 'name': 'mobile1', 'description':'black, 8gb RAM, 256gb Storage', 'rate': 12500},
+    {'id':2, 'name': 'mobile2', 'description':'black, 6gb RAM, 128gb Storage', 'rate': 13800},
+    {'id':3, 'name': 'mobile3', 'description':'black, 8gb RAM, 256gb Storage', 'rate': 14500},
+    {'id':4, 'name': 'mobile4', 'description':'black, 6gb RAM, 128gb Storage', 'rate': 15500},
+  ]
   constructor() { }
 
   ngOnInit(): void {
