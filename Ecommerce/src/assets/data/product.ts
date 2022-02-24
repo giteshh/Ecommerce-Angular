6267410c5e2f48bfa47270d76f53303a6f82c679
Gitesh Sharma <gitesh15796@gmail.com>
2022-02-24 12:18:13 +0530

    product description modified

--- Ecommerce/src/assets/data/product.ts
+++ Ecommerce/src/assets/data/product.ts
@@ -3,41 +3,52 @@ interface Product {
   name: string;
   price: number;
   description: string;
-  image: string
+  image: string;
+  mrp: number;
+  rating: number;
+  discount: number
 }
 
 const products = [
   {
     "id": 1,
     "name": "Samsung",
-    "mrp": 34999,
+    "mrp": 38399,
     "price": 31999,
     "description": "4 GB RAM 64GB ROM",
-    "image": "/assets/pics/samsung.jpg"
+    "image": "/assets/pics/samsung.jpg",
+    "rating": 3.8,
+    "discount": 20
   },
   {
     "id": 2,
     "name": "Apple Iphone",
-    "mrp": 79999,
+    "mrp": 78399,
     "price": 69999,
     "description": "6 GB RAM 64GB ROM",
-    "image": "/assets/pics/iphone.jpg"
+    "image": "/assets/pics/iphone.jpg",
+    "rating": 4.6,
+    "discount": 12
   },
   {
     "id": 3,
     "name": "Google Pixel",
-    "mrp": 49999,
+    "mrp": 43999,
     "price": 39999,
     "description": "4GB RAM 64GB ROM",
-    "image": "/assets/pics/pixel.jpg"
+    "image": "/assets/pics/pixel.jpg",
+    "rating": 4.0,
+    "discount": 10
   },
   {
     "id": 4,
     "name": "OnePlus 9 Pro",
-    "mrp": 89999,
+    "mrp": 87499,
     "price": 69999,
     "description": "8 GB RAM 256GB ROM",
-    "image": "/assets/pics/oneplus.jpg"
+    "image": "/assets/pics/oneplus.jpg",
+    "rating": 4.8,
+    "discount": 25
   }
 ]
 
