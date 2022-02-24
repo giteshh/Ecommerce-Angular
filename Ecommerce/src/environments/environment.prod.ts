0c471036b3fdfc30de7b29fadc8a0bc976674973
Gitesh Sharma <gitesh15796@gmail.com>
2022-02-24 20:49:02 +0530

    env file modified

--- Ecommerce/src/environments/environment.prod.ts
+++ Ecommerce/src/environments/environment.prod.ts
@@ -7,5 +7,14 @@ export const environment = {
   production,
   apiUrl,
   lastUpdatedOn,
-  envName
+  envName,
+  firebase: {
+    apiKey: "AIzaSyDy2x053SWKe8bWf9mpCHbhMnkcbVnqgN4",
+    authDomain: "ecommerce-a499f.firebaseapp.com",
+    projectId: "ecommerce-a499f",
+    storageBucket: "ecommerce-a499f.appspot.com",
+    messagingSenderId: "633670025323",
+    appId: "1:633670025323:web:45844cc4936c3da4e2f12e",
+    measurementId: "G-XJRZM9CYMT"
+  },
 };
